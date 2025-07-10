499a09518b017136b4f3abdef79d828a84ad1ea3
unknown <emmycool297@gmail.com>
2025-07-10 14:52:11 -0700

    frontend setup

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+   <>
+      <div className="bg-black text-white p-4 text-center">
+        welcome to our frontend mentorship program
+      </div>
+      </>
+  )
+}
+export default App
